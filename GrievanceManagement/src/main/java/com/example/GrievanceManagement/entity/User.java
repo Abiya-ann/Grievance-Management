a42e01f573f7c6ad1c51701843e0b9556b55d224
Abiya-ann <83870491+Abiya-ann@users.noreply.github.com>
2024-10-09 15:44:24 +0530

    Update User.java

--- GrievanceManagement/src/main/java/com/example/GrievanceManagement/entity/User.java
+++ GrievanceManagement/src/main/java/com/example/GrievanceManagement/entity/User.java
@@ -22,5 +22,6 @@ public class User {
     private String password;
 
     @OneToMany(mappedBy = "submittedBy", cascade = CascadeType.ALL)
+    @JsonIgnore
     private List<Grievance> grievances;
 }
